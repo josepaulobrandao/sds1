43c95678b8f0ab98deb4568c3b4f3ea6be641456
Jose Paulo <paulobrandao356@gmai.com>
2020-10-03 14:31:27 -0300

    Criando pagina home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 import './App.css';
 import './components/Header';
 import Header from './components/Header';
-
+import Home from './pages/Home';
 
 
 function App() {
   return (
-    <div>
-
+<div>
 <Header />
-    </div>
+<Home/>
+</div>
   );
 }
 
